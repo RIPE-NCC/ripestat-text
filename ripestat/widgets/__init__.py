b66ecb9a02c9c3ede1d8f606970c307000e8a1dd
Christopher Amin <camin@ripe.net>
2012-10-17 14:42:05 +0200

    Updated routing-status to work with prefixes

--- ripestat/widgets/__init__.py
+++ ripestat/widgets/__init__.py
@@ -30,6 +30,9 @@ GROUPS = {
 
 
 def get_group_widgets(group_name, resource_type):
+    """
+    Get a list of widgets for the given group and resource type.
+    """
     group_name = group_name.lower()
     widgets = []
     for widget in GROUPS.get(group_name, {}).get("widgets", []):
@@ -39,6 +42,10 @@ def get_group_widgets(group_name, resource_type):
 
 
 def get_widget(widget_name):
+    """
+    Return a text widget if one exists, otherwise return the default data
+    call wrapper widget.
+    """
     sanitized_name = widget_name.replace("-", "_").replace(" ", "_")
     try:
         module = __import__("ripestat.widgets.%s" % sanitized_name,
@@ -49,6 +56,9 @@ def get_widget(widget_name):
 
 
 def get_widget_list():
+    """
+    Get a list of lines describing every defined widget.
+    """
     rows = []
     for group in GROUPS.values():
         for widget in group["widgets"]:
@@ -59,14 +69,22 @@ def get_widget_list():
 
 
 def get_widget_groups():
+    """
+    Iterate over tuples of (widget group name, widget list).
+    """
     for group, definition in GROUPS.items():
         yield group, definition["widgets"]
 
 
 def default_widget(widget_name, api, query):
+    """
+    This 'widget' translates the response of the data call called 'widget_name'
+    in to a rudimentary whois format.
+    """
     data = api.get_data(widget_name, query)
     items = [
-        "'%s' doesn't have a command-line widget yet. Below is a direct translation of the data response." % widget_name,
+        "'%s' doesn't have a command-line widget yet. Below is a direct "
+        "translation of the data response." % widget_name,
         "You can contribute a widget at https://stat.ripe.net/cli/",
     ]
     title = widget_name
@@ -79,15 +97,21 @@ def default_widget(widget_name, api, query):
 
 
 def simple_table(rows):
+    """
+    For each sequence in 'rows', yield a string containing properly spaced
+    columns.
+    """
     # Calculate the column widths
     widths = None
+    rows = list(rows)
     for row in rows:
         if not isinstance(row, (list, tuple)):
             continue
         if widths is None:
             widths = [len(col) for col in row]
         else:
-            widths = [max(width, len(col)) for (width, col) in zip(widths, row)]
+            widths = [max(width, len(col)) for (width, col) in
+                zip(widths, row)]
 
     # Yield the rows
     for row in rows:
--- ripestat/widgets/routing_status.py
+++ ripestat/widgets/routing_status.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+from . import simple_table
+
 
 def widget(api, query):
     data = api.get_data("routing-status", query, version=1)
@@ -12,13 +14,32 @@ def widget(api, query):
         ("visibility", "{0:.0%}    {1} of {2} full peers".format(
             visibility, data["visibility"]["ris_peers_seeing"],
             data["visibility"]["total_ris_peers"])),
-        ("first-seen", data["first_seen"]["time"]),
-        ("announced-v4", "{announced_v4_prefixes} prefixes; {announced_v4_ips}"
-            " IPs".format(**data)),
-        ("announced-v6", "{announced_v6_prefixes} prefixes; equivalent to "
-            "{announced_v6_48s} /48{0}".format("s" if data["announced_v6_48s"]
-            > 1 else "", **data)),
-        ("bgp-neighbours", "{observed_neighbours}".format(**data)),
     ]
+
+    if data.get("first_seen"):
+        result.append(("first-seen", data["first_seen"]["time"]))
+    else:
+        result.append(("first-seen", "never"))
+
+    if "announced_v4_prefixes" in data:
+        result.append(("announced-v4", "{announced_v4_prefixes} prefixes; "
+            "{announced_v4_ips} IPs".format(**data)))
+    if "announced_v6_prefixes" in data:
+        result.append(("announced-v6", "{announced_v6_prefixes} prefixes; "
+            "equivalent to {announced_v6_48s} /48{0}".format("s" if
+            data["announced_v6_48s"] > 1 else "", **data)))
+    if "observed_neighbours" in data:
+        result.append((("bgp-neighbours", "{observed_neighbours}".format(
+            **data))))
+
+    get_table = lambda suggestions: simple_table((s["prefix"], "announced by",
+        str(s["origin"])) for s in suggestions)
+
+    for suggestion in get_table(data.get("less_specifics", [])):
+        result.append(("less-specific", suggestion))
+
+    for suggestion in get_table(data.get("more_specifics", [])):
+        result.append(("more-specific", suggestion))
+
 
     return result
