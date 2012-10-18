1829899ac06b2752464b034a0ae4fcc177ef5ed6
Christopher Amin <camin@ripe.net>
2012-10-18 14:03:45 +0200

    Updated URL in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='ripestat-text',
       version=VERSION,
       description='Text mode utils for RIPEstat',
       author_email='stat@ripe.net',
-      url='https://stat.ripe.net/cli/',
+      url='https://github.com/RIPE-NCC/ripestat-text',
       packages=find_packages(),
       scripts=["scripts/ripestat", "scripts/ripestat-whois-server"]
      )
