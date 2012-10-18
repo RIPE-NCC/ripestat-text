31d2de80fd516f559bf543fb753e2694a9ae2d1f
Christopher Amin <camin@ripe.net>
2012-10-18 13:14:37 +0200

    Added IPv6 support to whois server

--- ripestat/whois/server.py
+++ ripestat/whois/server.py
@@ -3,27 +3,48 @@ Module containing a ripestat-text whois server.
 
 The executable script for the whois service lives at scripts/ripestat-whois.
 """
-from getopt import gnu_getopt
+from optparse import OptionParser
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
-from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
 from ripestat.api import StatAPI
 from ripestat.core import StatCore
 
 
+class StatWhoisServer(object):
+    """
+    Class for handling whois interaction with StatCore.
+    """
+    parser = OptionParser()
+    parser.add_option("-p", "--port", default="43")
+    parser.add_option("-b", "--base-url",
+        default="https://stat.ripe.net/data/")
+    parser.add_option("-i", "--interface", action="append" )
+
+    def __init__(self, params):
+        self.options, self.args = self.parser.parse_args(params)
+
+    def start(self):
+        """
+        Start listening on the given port.
+        """
+        factory = WhoisFactory(self.options.base_url)
+        for interface in (self.options.interface or ["", "::"]):
+            reactor.listenTCP(int(self.options.port), factory,
+                interface=interface)
+        reactor.run()
+
+
 class WhoisProtocol(LineReceiver):
     """
     Twisted protocol that passes I/O between the user and StatCore.
     """
-
     def lineReceived(self, line):
         """
         Parse a line of user input and pass it to StatCore.
         """
-        api = StatAPI("whois", base_url=self.factory.base_url)
-        core = StatCore(self.script_output, api=api)
+        core = StatCore(self.script_output, api=self.factory.api)
         core.main(line.split())
         self.transport.loseConnection()
 
@@ -40,21 +61,5 @@ class WhoisFactory(Factory):
     """
     protocol = WhoisProtocol
 
-
-class StatWhoisServer(object):
-    """
-    Class for handling whois interaction with StatCore.
-    """
-
-    def __init__(self, params):
-        self.options, self.args = gnu_getopt(params, "")
-
-    def start(self):
-        """
-        Start listening on the given port.
-        """
-        endpoint = TCP4ServerEndpoint(reactor, int(self.args[0]))
-        factory = WhoisFactory()
-        factory.base_url = self.args[1]
-        endpoint.listen(factory)
-        reactor.run()
+    def __init__(self, base_url):
+        self.api = StatAPI("whois", base_url)
