4b84fb6a029f4cb69e60e1d7b7c3cc347d62fde0
Christopher Amin <camin@ripe.net>
2012-10-29 18:07:18 +0100

    Made a Twisted Application for the server

--- ripestat/server.py
+++ ripestat/server.py
@@ -3,13 +3,13 @@ Module containing a ripestat-text whois-style server.
 
 The executable script for the whois service lives at scripts/ripestat-whois.
 """
-from optparse import OptionParser
+from optparse import make_option
 import logging
+from Queue import Queue
 
 from twisted.internet import reactor
-from twisted.internet.error import CannotListenError
 from twisted.internet.protocol import Factory
-from twisted.protocols.basic import LineReceiver
+from twisted.protocols.basic import LineOnlyReceiver
 
 from ripestat.api import StatAPI
 from ripestat.core import StatCore, StatCoreParser
@@ -19,48 +19,102 @@ logging.basicConfig()
 LOG = logging.getLogger(__name__)
 
 
-class StatTextServer(object):
+class StatTextProtocol(LineOnlyReceiver):
     """
-    Class for handling whois-style interaction with StatCore.
-                LOG.exception(exc)
+    Twisted protocol that passes I/O between the client and StatCore.
     """
-    parser = OptionParser()
-    parser.add_option("-p", "--port", default="43")
-    parser.add_option("-b", "--base-url",
-        default="https://stat.ripe.net/data/")
-    parser.add_option("-i", "--interface", action="append" )
+    delimiter = "\n"
 
-    def __init__(self, params):
-        self.options, self.args = self.parser.parse_args(params)
+    def connectionMade(self):
+        """
+        Initialize state when the client connects.
+        """
+        # Get the reader instance for this protocol
+        readers = reactor.getReaders()
+        for reader in readers:
+            if getattr(reader, "protocol", None) == self:
+                self.reader = reader
+                break
+        self.keep_alive = False
+        self.input_lines = Queue()
 
-    def start(self):
+    def dataReceived(self, data):
         """
-        Start the server with the provided options.
+        Overridden to stop trying to read data while outputting a response.
+
+        This stops netcat from quitting before it gets the output!
         """
-        factory = WhoisFactory(self.options.base_url)
-        success = False
-        for interface in (self.options.interface or ["::"]):
-            try:
-                reactor.listenTCP(int(self.options.port), factory,
-                    interface=interface)
-            except CannotListenError as exc:
-                LOG.error(exc)
-            else:
-                success |= True
-        if success:
-            reactor.run()
+        reactor.removeReader(self.reader)
+        retval = LineOnlyReceiver.dataReceived(self, data)
+        reactor.callInThread(self.processLines)
+        return retval
+
+    def lineReceived(self, line):
+        """
+        Parse a line of user input and render the widgets.
+        """
+        self.input_lines.put(line)
+
+    def processLines(self):
+        """
+        Render a set of widgets for each input line on the queue.
+        """
+        while self.input_lines.qsize():
+            line = self.input_lines.get()
+            self.renderWidgets(line)
+            self.input_lines.task_done()
+
+        # Maintain or end the connection depending on the mode
+        if self.keep_alive:
+            reactor.callFromThread(reactor.addReader, self.reader)
         else:
-            LOG.error("Couldn't listen on any ports. Exiting.")
-            return -1
+            reactor.callFromThread(self.transport.loseConnection)
+
+    def renderWidgets(self, line):
+        """
+        Execute the appropriate widgets and queue the output for sending from
+        the main thread.
+        """
+        params = line.strip().split()  # We need to accept trailing \r
+
+        parser = StatTextLineParser(self)
+        options, args = parser.parse_args(params)
+
+        # Render the widgets if the input wasn't a single keep_alive flag
+        if not (options.keep_alive and not args):
+            core = StatCore(self.queueLine, api=self.factory.api,
+                parser=parser)
+            core.main(params)
+
+        if options.keep_alive:
+            self.keep_alive = not self.keep_alive
+
+    def queueLine(self, line):
+        """
+        Callback method to allow StatCore to send output over the network.
+
+        Each line is queued in the main thread for sending.
+        """
+        reactor.callFromThread(self.sendLine, line.encode("utf-8"))
 
 
-class WhoisLineParser(StatCoreParser):
+class StatTextFactory(Factory):
+    """
+    Twisted factory that uses the StatTextProtocol.
+    """
+    protocol = StatTextProtocol
+
+    def __init__(self, base_url):
+        self.api = StatAPI("whois", base_url)
+
+
+class StatTextLineParser(StatCoreParser):
     """
     StatCoreParser subclass that responds to input from whois clients.
     """
     whois_option_list = [
-        # make_option("-k", "--keep-alive", action="store_true",
-        #    help="use a persistent connection")
+        make_option("-k", "--keep-alive", action="store_true",
+            help="use a persistent connection")
     ]
 
     def __init__(self, protocol, *args, **kwargs):
@@ -81,64 +135,3 @@ class WhoisLineParser(StatCoreParser):
         The whois parser should never exit.
         """
         pass
-
-
-class WhoisProtocol(LineReceiver):
-    """
-    Twisted protocol that passes I/O between the client and StatCore.
-    """
-    delimiter = "\n"
-
-    def connectionMade(self):
-        """
-        Initialize state when the client connects.
-        """
-        # Get the reader instance for this protocol
-        readers = reactor.getReaders()
-        for reader in readers:
-            if getattr(reader, "protocol", None) == self:
-                self.reader = reader
-                break
-
-    def lineReceived(self, line):
-        """
-        Parse a line of user input and pass it to StatCore.
-        """
-        # Don't accept any more data yet
-        # This stops netcat from quitting before it gets the output!
-        reactor.removeReader(self.reader)
-        # Render the widgets in a separate thread
-        reactor.callInThread(self.respond_to_client, line)
-
-    def respond_to_client(self, line):
-        """
-        Execute the appropriate widgets and queue the output for sending from
-        the main thread.
-        """
-        params = line.strip().split()  # We need to accept trailing \r
-
-        parser = WhoisLineParser(self)
-        options, args = parser.parse_args(params)
-
-        core = StatCore(self.output, api=self.factory.api, parser=parser)
-        core.main(params)
-
-        reactor.callFromThread(self.transport.loseConnection)
-
-    def output(self, line):
-        """
-        Callback method to allow StatCore to send output over the network.
-
-        Each line is queued in the main thread for sending.
-        """
-        reactor.callFromThread(self.sendLine, line.encode("utf-8"))
-
-
-class WhoisFactory(Factory):
-    """
-    Twisted factory that uses the WhoisProtocol.
-    """
-    protocol = WhoisProtocol
-
-    def __init__(self, base_url):
-        self.api = StatAPI("whois", base_url)
